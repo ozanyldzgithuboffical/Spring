c297f29ffa416eb22c91c7523e296d1db39dccc2
Ozan YILDIZ <53907221+ozanyldzgithuboffical@users.noreply.github.com>
2019-10-14 22:43:28 +0300

    Add files via upload
    checkbox is implemented with a model

---
+++ Customer.java
@@ -0,0 +1,60 @@
+package ozan.yldz.spring.mvc;
+
+import java.util.LinkedHashMap;
+
+public class Customer {
+private String name;
+private String surName;
+private String phoneNumber;
+private String gender;
+private String country;
+private LinkedHashMap<String, String> countries;
+private String [] hobby;
+//Constructor
+public Customer() {
+	countries=new LinkedHashMap<String,String>();
+	countries.put("Turkey", "Turkey");
+	countries.put("Germany", "Germany");
+	countries.put("France", "France");
+	countries.put("Spain", "Spain");
+}
+
+//Getters & Setters
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSurName() {
+	return surName;
+}
+public void setSurName(String surName) {
+	this.surName = surName;
+}
+public String getPhoneNumber() {
+	return phoneNumber;
+}
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+public String getGender() {
+	return gender;
+}
+public void setGender(String gender) {
+	this.gender = gender;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+public LinkedHashMap<String, String> getCountries() {
+	return countries;
+}
+public void setCountries(LinkedHashMap<String, String> countries) {
+	this.countries = countries;
+}
+
+}
---
+++ form.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Spring Demo Basic</title>
+</head>
+<body>
+<h2>Welcome to Ozan's Spring MVC Tutorial Series</h2>
+ <form:form action="processForm"  modelAttribute="" >
+ 	<form:input path="name"/>
+ 	<br>
+ 	<form:input path="surName"/>
+ 	<br>
+ 	<form:input path="phoneNumber"/>
+ 	<br>
+ 	<form:select path="country">
+ 	<form:options items="${customer.countries}" />
+ 	</form:select> 
+ 	Hobby Fields
+ 	Swimming <form:checkbox path="hobby"/>
+ 	<br>
+ 	Basketball <form:checkbox path="hobby"/>
+ 	<br>
+ 	Reading <form:checkbox path="hobby"/>
+ 	<br>
+ 	Volleyball <form:checkbox path="hobby"/>
+ </form:form>
+<input type="submit"/>
+</form>
+</body>
+</html>
